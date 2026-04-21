af8d16a6fc0841078122b8f6084dc5bb2ccef375
声声 <1175235190@qq.com>
2026-04-21 23:27:01 +0800

    Change annotation in the code

--- AITools/AppShell.xaml.cs
+++ AITools/AppShell.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            // 确保路由已注册
+            // Make sure the route has been registered.
             Routing.RegisterRoute("modelsquare", typeof(Views.modelSquare));
             Routing.RegisterRoute("ai", typeof(Views.AI));
             Routing.RegisterRoute("myself", typeof(Views.Myself));
